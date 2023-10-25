e6cca63095af289748ada879291fe289f2761e4f
Anna <anmaklos@gmail.com>
2023-10-25 12:43:25 +0200

    folder structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <p>Dashboard page (protected)</p>
+      <Button>Click</Button>
+    </>
+  );
+};
+
+export default DashboardPage;
